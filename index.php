1454eed214d224469159b7746f17338a05e8b729
Kudo <phuangpila101@hotmail.com>
2017-03-09 15:23:58 +0700

    m
    m

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Form</title>
+  
+    <link rel="stylesheet" href="assets/login/normalize.min.css">
+    <link rel="stylesheet" href="assets/login/style.css">
+    <script src="assets/login/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+  <div class="login">
+    <h1>Login</h1>
+    <form method="post" action="admin_index.php">
+        <input type="text" name="u" placeholder="Username" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">Login</button>
+    </form>
+</div>
+  
+    <script src="js/index.js"></script>
+
+</body>
+</html>
