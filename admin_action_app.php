1ebf64ff128fa305252ffb091bc8c5e426786e45
Kudo <phuangpila101@hotmail.com>
2017-03-29 15:16:02 +0700

    ก
    ก

--- admin_action_app.php
+++ admin_action_app.php
@@ -3,7 +3,7 @@ include('include/connect.php');
 include('include/function.php');
 if($_GET['ok']=='1'){
 $newdata = array(
-"status"=>"1",
+"status"=>"1",  
 );
 update("appoint",$newdata,"id = '".$_GET['app_id']."' ");
 header("refresh:0.5; admin_index.php?menu=app" );
